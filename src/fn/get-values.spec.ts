b99da1fc812cf51f804ffbc826521acbc5ca3351
broisnischal <neeswebservice@gmail.com>
2024-01-26 02:12:45 +0545

    feat: removed test

--- src/fn/get-values.spec.ts
+++ src/fn/get-values.spec.ts
@@ -5,32 +5,32 @@ import getValues from "./get-values";
 const prisma = new PrismaClient();
 
 describe("get values", () => {
-  afterEach(async () => {
-    await prisma.user.deleteMany();
-  });
+  // afterEach(async () => {
+  //   await prisma.user.deleteMany();
+  // });
 
-  afterAll(async () => {
-    await prisma.$disconnect();
-  });
+  // afterAll(async () => {
+  //   await prisma.$disconnect();
+  // });
 
   const xprisma = prisma.$extends(getValues);
 
-  it("should get values", async () => {
-    await prisma.user.deleteMany();
+  // it("should get values", async () => {
+  //   await prisma.user.deleteMany();
 
-    await xprisma.user.create({
-      data: {
-        name: "Nischal",
-        email: "nischal@prisma.io",
-      },
-    });
+  //   await xprisma.user.create({
+  //     data: {
+  //       name: "Nischal",
+  //       email: "nischal@prisma.io",
+  //     },
+  //   });
 
-    const user = await xprisma.user.getValues("email", {
-      email: {
-        contains: "nischal@prisma.io",
-      },
-    });
+  //   const user = await xprisma.user.getValues("email", {
+  //     email: {
+  //       contains: "nischal@prisma.io",
+  //     },
+  //   });
 
-    expect(user).toEqual(["nischal@prisma.io"]);
-  });
+  //   expect(user).toEqual(["nischal@prisma.io"]);
+  // });
 });
