cba1e3c8a32469eb8c67e2f01769486130b553b6
Rojan Rana Magar <rojan.magar@aitc.ai>
2024-02-09 18:54:36 +0545

    chore: changes

--- src/fn/exists.ts
+++ src/fn/exists.ts
@@ -1,19 +1,21 @@
 import { Prisma } from "@prisma/client";
 
 export default Prisma.defineExtension((prisma) => {
-	return prisma.$extends({
-		name: "exists",
-		model: {
-			$allModels: {
-				async exists<T>(this: T, where: Prisma.Args<T, "findFirst">["where"]) {
-					const context = Prisma.getExtensionContext(this);
+  return prisma.$extends({
+    name: "exists",
+    model: {
+      $allModels: {
+        async exists<T>(this: T, where: Prisma.Args<T, "findFirst">["where"]) {
+          const context = Prisma.getExtensionContext(this);
 
-					const result = await (context as any).findFirst({
-						where,
-					});
-					return result !== null;
-				},
-			},
-		},
-	});
+          const result = await (context as any).findFirst({
+            where,
+          });
+          return result !== null;
+        },
+      },
+    },
+  });
 });
+
+// This
